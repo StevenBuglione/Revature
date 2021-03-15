baa12b3c08839126db0c6158a45738a33f135185
Steven Buglione <stevenbuglione1@gmail.com>
2021-03-15 11:16:39 -0400

    Week One Exercises Added

---
+++ WeekOne/ExerciseOne.java
@@ -0,0 +1,11 @@
+package WeekOne;
+
+public class ExerciseOne {
+
+    public static void main(String[] args){
+        double number1 = 10.5;
+        double number2 = 11.5;
+        double result = number1*number2;
+        System.out.print(result);
+    }
+}
